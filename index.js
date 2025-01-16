d6a72aa828c39129ac2ba0ea1b919761ffe4bd24
Luis Azcuaga <37643667+luisAzcuaga@users.noreply.github.com>
2025-01-16 07:18:27 -0600

    add PWA support

--- index.js
+++ index.js
@@ -48,6 +48,17 @@ document.addEventListener('DOMContentLoaded', () => {
       circleBreathing(BREATHING_TIME_INTERVAL_MILLIS);
     }
   })
+
+  
+  // Reference the serviceWorker.
+  const serviceWorker = navigator.serviceWorker;
+  // Register our ServiceWorker script, if serviceWorker is available.
+  if (serviceWorker) {
+    serviceWorker
+      .register("/service-worker.js")
+      .then(() => console.log("ServiceWorker Registered to the Application!"))
+      .catch((e) => console.log("Failed to Register the ServiceWorker.", e));
+    }
 });
 
 const stopGuide = () => {
---
+++ service-worker.js
@@ -0,0 +1,28 @@
+// Name of the Cache.
+const CACHE = "mindfulnessAssistantCache_V1";
+
+// Select files for caching.
+let urlsToCache = [
+  "/",
+  "/index.html",
+  "/index.js",
+  "/index.css",
+  "/favicon.png", 
+];
+
+// Cache all the selected items once application is installed.
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE)
+    .then((cache) => (cache.addAll(urlsToCache)))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request)
+    .then((response) => {
+      if (response) return response;
+
+      return fetch(event.request);
+    })
+  );
+});
